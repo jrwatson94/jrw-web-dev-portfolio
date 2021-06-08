41d1b643d0d3f8599bc6708795d9e200dd67c569
jrwatson94 <j.watsonreid@gmail.com>
2021-06-08 19:54:56 -0400

    about images added

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,23 @@
 import React from 'react';
+import css from '../assets/logos/css.png';
+import postgres from '../assets/logos/postgres.png';
+import ruby from '../assets/logos/ruby.png';
+import js from '../assets/logos/javascript.png';
+import html from '../assets/logos/html-5.png';
+import react from '../assets/logos/react.png';
+import redux from '../assets/logos/redux.png';
+import bootstrap from '../assets/logos/bootstrap.png';
+import semantic from '../assets/logos/semantic.png';
+import rails from '../assets/logos/rails.png';
+import github from '../assets/logos/github.png';
+import medium from '../assets/logos/medium.png';
+import linkedin from '../assets/logos/linkedin.png';
+
+
+
+
+
+
 
 function About (){
     return(
@@ -19,24 +38,24 @@ function About (){
                     <br></br>
                     <h4><u>Languages</u></h4>
                     <div className="row justify-content-center text-center">
-                        <div className="col-6 col-md-4 col-lg-2">SQL</div>
-                        <div className="col-6 col-md-4 col-lg-2">Ruby</div>
-                        <div className="col-6 col-md-4 col-lg-2">Javascript</div>
-                        <div className="col-6 col-md-4 col-lg-2">HTML</div>
-                        <div className="col-6 col-md-4 col-lg-2">CSS</div>
+                        <div className="col-6 col-md-4 col-lg-2"><img className="code-logo" src={ruby}></img></div>
+                        <div className="col-6 col-md-4 col-lg-2"><img id="postg" className="code-logo" src={postgres}></img></div>
+                        <div className="col-6 col-md-4 col-lg-2"><img className="code-logo" src={js}></img></div>
+                        <div className="col-6 col-md-4 col-lg-2"><img className="code-logo" src={html}></img></div>
+                        <div className="col-6 col-md-4 col-lg-2"><img className="code-logo" src={css}></img></div>
                     </div>
                     <h4><u>Frameworks</u></h4>
                     <div className="row justify-content-center text-center">
-                        <div className="col-6 col-md-4 col-lg-2">Rails</div>
-                        <div className="col-6 col-md-4 col-lg-2">React</div>
-                        <div className="col-6 col-md-4 col-lg-2">Redux</div>
-                        <div className="col-6 col-md-4 col-lg-2">Bootstrap</div>
-                        <div className="col-6 col-md-4 col-lg-2">Semantic UI</div>
+                        <div className="col-6 col-md-4 col-lg-2"><img className="code-logo" src={rails}></img></div>
+                        <div className="col-6 col-md-4 col-lg-2"><img className="code-logo" src={react}></img></div>
+                        <div className="col-6 col-md-4 col-lg-2"><img className="code-logo" src={redux}></img></div>
+                        <div className="col-6 col-md-4 col-lg-2"><img className="code-logo" src={bootstrap}></img></div>
+                        <div className="col-6 col-md-4 col-lg-2"><img className="code-logo" src={semantic}></img></div>
                     </div>
                     <div className="row justify-content-center text-center about-links">
-                        <div className="col-12 col-md-4">Github</div>
-                        <div className="col-12 col-md-4">LinkedIn</div>
-                        <div className="col-12 col-md-4">Blog</div>
+                        <div className="col-12 col-md-4"><i className="fab fa-github"></i></div>
+                        <div className="col-12 col-md-4"><i className="fab fa-linkedin"></i></div>
+                        <div className="col-12 col-md-4"><i className="fab fa-medium"></i></div>
                     </div>
                 </div>
 
